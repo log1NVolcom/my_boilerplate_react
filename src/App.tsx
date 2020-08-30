0b4f2ec6fb32ccc86d6dd76595753dccc650e0eb
log1NVolcom <joaorego_5@hotmail.com>
2020-08-30 15:12:07 +0100

    removed yarn.lock and junk

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,5 @@ import React from 'react';
 
 import './App.scss';
 
-const App: React.FC = () => {
-  const a = 'asda';
-  return <div>My project</div>;
-};
+const App: React.FC = () => <div>My project</div>;
 export default App;
